c1692ec90fa8d0e780a5c4013002215358344ab6
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-17 02:30:52 +0530

    add to user_header.php

---
+++ components/user_header.php
@@ -0,0 +1,16 @@
+<section class="header">
+      <div class="flex">
+        <a href="#home" class="logo">Hotels and resturants</a>
+        <a href="#availability" class="btn">check availability</a>
+        <div id="menu-btn" class="fas fa-bars"></div>
+      </div>
+
+      <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#reservation">reservation</a>
+        <a href="#gallery">gallery</a>
+        <a href="#contact">contact</a>
+        <a href="#reviews">reviews</a>
+      </nav>
+    </section>
