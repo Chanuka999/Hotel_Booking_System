cba7f661c780f94e17d3a9be205771fe6b7e03ef
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-17 02:30:34 +0530

    add to footer file

---
+++ components/footer.php
@@ -0,0 +1,32 @@
+<section class="footer">
+      <div class="box-container">
+        <div class="box">
+          <a href="tel-123456789"><i class="fs fa-phone"></i>+123-456-7890</a>
+          <a href="tel-111222333"><i class="fs fa-phone"></i>+111-222-3333</a>
+          <a href="mailt0:chanuka@gmail.com"
+            ><i class="fs fa-envelope"></i>chanuka@gmail.com</a
+          >
+          <a href="#"><i class="fs fa-map-marker"></i>Sri lanka</a>
+        </div>
+
+        <div class="box">
+          <a href="#home">home</a>
+          <a href="#about">about</a>
+          <a href="#reservation">reservation</a>
+          <a href="#gallery">gallery</a>
+          <a href="#contact">contact</a>
+          <a href="#reviews">reviews</a>
+        </div>
+
+        <div class="box">
+          <a href="#">facebook<i class="fab fa-facebook"></i></a>
+          <a href="#">twitter<i class="fab fa-twitter"></i></a>
+          <a href="#">instagram<i class="fab fa-instagram"></i></a>
+          <a href="#">linkdin<i class="fab fa-linkdin"></i></a>
+        </div>
+      </div>
+
+      <div class="credit">
+        &copy; copyright @ 2024 CHANUKA RANDITHA | all right reserved!
+      </div>
+    </section>
